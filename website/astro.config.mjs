ead81dab33064e1a5ecd9b6d80b088bd65a60f69
Your Name <you@example.com>
2026-04-18 21:40:02 +0800

    init

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://conradbez.github.io',
+  base: '/ocd-trial',  // replace with your exact GitHub repo name
+});
